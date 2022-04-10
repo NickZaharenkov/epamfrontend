54252a2542698f67aa90c3fec25924e6279e3d9e
NickZaharenkov <nickwolf0377@gmail.com>
2022-04-10 20:25:28 +0300

    without modules

---
+++ Angular #4/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { User } from './models/user.model';
+import { UserService } from './services/user.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'Angular Test CLI';
+
+  name: string;
+  age: number;
+
+  users: User[] = [];
+
+  constructor(private userService: UserService) {
+    this.users = this.userService.getUsers();
+  }
+
+
+  addUser(): void {
+      this.users.push(new User(this.name, this.age))
+      this.clearUser();
+  }
+  clearUser(): void {
+    this.name = '';
+    this.age = null;
+  }
+}
